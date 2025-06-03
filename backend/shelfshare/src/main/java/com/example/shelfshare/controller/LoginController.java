5802f710cff8897517837f43b357cebd5ed4abde
Harshita Padmanabhuni <pvgharshita@google.com>
2025-06-03 12:10:06 +0530

    Resolved CORS error

--- backend/shelfshare/src/main/java/com/example/shelfshare/controller/LoginController.java
+++ backend/shelfshare/src/main/java/com/example/shelfshare/controller/LoginController.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,6 +23,7 @@ import jakarta.servlet.http.HttpServletResponse;
 
 @RestController
 @RequestMapping("/login")
+@CrossOrigin(origins = "http://localhost:5173")
 public class LoginController {
 
     @Autowired
