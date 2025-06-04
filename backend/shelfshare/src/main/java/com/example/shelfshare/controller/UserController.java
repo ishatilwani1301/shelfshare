9cd632ad98b962a5584a896b8ea60d850aecc2c7
Harshita Padmanabhuni <pvgharshita@google.com>
2025-06-04 12:29:25 +0530

    Added end point for changing user password

--- backend/shelfshare/src/main/java/com/example/shelfshare/controller/UserController.java
+++ backend/shelfshare/src/main/java/com/example/shelfshare/controller/UserController.java
@@ -12,6 +12,11 @@ import org.springframework.web.bind.annotation.RestController;
 import com.example.shelfshare.model.UserDetailsResponse;
 import com.example.shelfshare.service.UserService;
 
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import com.example.shelfshare.model.UserPasswordChangeRequest;
+import com.example.shelfshare.model.UserPasswordChangeResponse;
 
 @RestController
 @RequestMapping("/user")
@@ -42,6 +47,44 @@ public class UserController {
                 userDetails.getUserEmail(),
                 userDetails.getPincode()
             ), HttpStatus.OK);
+        }
+    }
+
+    @PostMapping("/changeUserPassword")
+    public ResponseEntity<UserPasswordChangeResponse> userPasswordChange(@RequestBody UserPasswordChangeRequest request, Principal principal) {
+        if (principal == null) {
+            return new ResponseEntity<UserPasswordChangeResponse>(
+                new UserPasswordChangeResponse("User not authenticated"),
+                HttpStatus.UNAUTHORIZED
+            );
+        } else {
+            var user = userService.getUserByUsername(principal.getName());
+            if (user.isEmpty()) {
+                return new ResponseEntity<UserPasswordChangeResponse>(
+                    new UserPasswordChangeResponse("User not found"),
+                    HttpStatus.NOT_FOUND
+                );
+            }
+
+            var userInDb = user.get();
+            if (!userService.checkPassword(userInDb, request.oldPassword())) {
+                return new ResponseEntity<UserPasswordChangeResponse>(
+                    new UserPasswordChangeResponse("Old password is incorrect"),
+                    HttpStatus.BAD_REQUEST
+                );
+            }
+
+            if (userService.changePassword(userInDb, request.newPassword())) {
+                return new ResponseEntity<UserPasswordChangeResponse>(
+                    new UserPasswordChangeResponse("Password changed successfully"),
+                    HttpStatus.OK
+                );
+            } else {
+                return new ResponseEntity<UserPasswordChangeResponse>(
+                    new UserPasswordChangeResponse("Failed to change password"),
+                    HttpStatus.INTERNAL_SERVER_ERROR
+                );
+            }
         }
     }
 }
