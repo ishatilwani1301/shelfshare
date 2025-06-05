1fb120132b6fddc2ab0ce85bdb24d13f17fc1101
Harshita Padmanabhuni <pvgharshita@google.com>
2025-06-05 22:23:30 +0530

    Changed the end points for User details, password editing, editing user details, delete user, add book

--- backend/shelfshare/src/main/java/com/example/shelfshare/model/UserDetailsResponse.java
+++ backend/shelfshare/src/main/java/com/example/shelfshare/model/UserDetailsResponse.java
@@ -1,3 +1,3 @@
 package com.example.shelfshare.model;
 
-public record  UserDetailsResponse(String username, String email, String pincode) {};
+public record  UserDetailsResponse(String name, String username, String email, String pincode, String area, String city, String state, String country) {};
